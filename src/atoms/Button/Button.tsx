461fe70b013ba88c1e109a8250e55fb53bfce4bd
shreyasharmaweb <shreyasharma7051@gmail.com>
2024-10-27 14:42:25 +0530

    WIP

---
+++ src/atoms/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button style={{ backgroundColor: 'blue', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px' }}>
+      Click Me
+    </button>
+  );
+};
+
+export default Button;
