6e959cfedd97393240eac1f95238b4922f8173c9
hdrayka9060 <hdrayka9060@gmail.com>
2024-11-04 13:12:54 +0530

    added atoms/icons/types

---
+++ src/atoms/Icons/types.d.ts
@@ -0,0 +1,10 @@
+import { Sizes, Colors, AlignType } from "../../typings";
+
+export interface IconProps {
+  width?: Sizes;
+  height?: Sizes;
+  fillPath?: string;
+  align?: AlignType;
+  fill?: Colors;
+  stroke?: Colors;
+}
